2bc167011712a8e84c7e087f73565f19ddfc6a60
Ole Zorn <olezorn@gmx.net>
2011-01-16 01:41:10 +0100

    Removed logs

--- PBGitRepository.m
+++ PBGitRepository.m
@@ -1140,7 +1140,7 @@ NSString* PBGitRepositoryErrorDomain = @"GitXErrorDomain";
 
 - (void) finalize
 {
-	NSLog(@"Dealloc of repository");
+	//NSLog(@"Dealloc of repository");
 	[super finalize];
 }
 @end
